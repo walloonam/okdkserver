0d2ed71cf5ebc01271bf8f4f9dbf53bc9bc1a7c3
jomulagy <jomulagy988@gmail.com>
2023-04-13 15:02:53 +0900

    얼굴인식 얼굴등록 분리

--- src/main/java/okdk/okdkserver/app/keyosk/servercontroller.java
+++ src/main/java/okdk/okdkserver/app/keyosk/servercontroller.java
@@ -2,6 +2,7 @@ package okdk.okdkserver.app.keyosk;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import okdk.okdkserver.app.keyosk.PythonSocet.PythonSocketForRecognization;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,12 +17,30 @@ public class servercontroller {
     @PostMapping(value = "/server/image",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public String saveImage(@RequestParam MultipartFile imgFile) throws IOException {
         System.out.println(imgFile);
-        if(!imgFile.isEmpty()){
+        String result = null;
+        if (!imgFile.isEmpty()) {
             String fullPath = "C:/okdkserver/image/" + imgFile.getOriginalFilename();
-            System.out.println("파일 저장 full path = "+ fullPath);
+            System.out.println("파일 저장 full path = " + fullPath);
             imgFile.transferTo(new File(fullPath));
+
+            //모델로 이미지 경로 보내기
+            PythonSocketForRecognization AIModel = new PythonSocketForRecognization();
+            result = AIModel.face_recognize_result(fullPath);
+
+            //김지영이랑 통신하는 함수에서 추가해야 될것
+            //PythonSocketForRegister AIModel = new PythonSocketForRegister();
+            //result = AIModel.face_recognize_result(fullPath);
+
         }
-        return "test-form";
+        //인식 결과 내보내기
+        return result;
+    }
+    @GetMapping(value = "/server/image/test")
+    public String saveImagetest() throws IOException {
+        PythonSocketForRecognization python_test_class = new PythonSocketForRecognization();
+        String result = python_test_class.face_recognize_result("hi");
+        System.out.println(result);
+        return "ok";
     }
 
     @PostMapping(value = "/arduino/json")
